a3abb8af5caee8fa01758c43060f7e5cf38764da
Revati <Revati@DESKTOP-H46ULN5>
2022-02-05 20:38:38 +0530

    commit

--- src/JavaAssignment/A1Q1.java
+++ src/JavaAssignment/A1Q1.java
@@ -1,5 +1,18 @@
 package JavaAssignment;
+import java.util.Scanner;
 
-public class A1Q1 {
+public class A1Q1{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number- ");  
+        int a= sc.nextInt();        
 
-}
+        int reverse = 0;
+        while (a != 0) {
+            int remainder = a % 10;
+            reverse = reverse * 10 + remainder;
+            a = a / 10;
+        }
+        System.out.println("The reverse of the given number is: " + reverse);
+    }
+
